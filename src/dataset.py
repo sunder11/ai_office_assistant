55cba6d9a4472c09f74def51818ea46c4dfe89e3
sunder11 <steve@underwoodlaw.net>
2025-06-16 12:45:58 -0400

    Modify Law_Agent to what I want to do

--- src/dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Sample DataFrame
-data = {"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]}
-df = pd.DataFrame(data)
-
-# Get 2 random rows and test and testing
-sample_df = df.sample(n=2)
-print(sample_df)
