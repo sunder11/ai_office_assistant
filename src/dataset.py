e1c80d7f3ea194aa47b476b1065b4142fc07ea3b
Stephen K Underwood <steve@underwoodlaw.net>
2025-06-16 04:30:34 -0400

    Added Test Jupyter In Sidebar Dataset

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Sample DataFrame
+data = {"col1": [1, 2, 3, 4, 5], "col2": ["a", "b", "c", "d", "e"]}
+df = pd.DataFrame(data)
+
+# Get 2 random rows
+sample_df = df.sample(n=2)
+print(sample_df)
